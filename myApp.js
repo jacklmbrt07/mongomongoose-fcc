345b4dab00acc293019b872405257152e9758925
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-04 11:03:03 -0800

    completed challenge 7/12

--- myApp.js
+++ myApp.js
@@ -54,21 +54,24 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  Person.find({name: personName}, (err, data) => {
+  Person.find({ name: personName }, (err, data) => {
     if (err) return console.log(err);
     done(null, data);
-  })
+  });
 };
 
 const findOneByFood = (food, done) => {
-  Person.findOne({favoriteFoods: food}, (err, data) => {
-    if(err) return console.log(err)
-    done(null, data)
-  })
+  Person.findOne({ favoriteFoods: food }, (err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
