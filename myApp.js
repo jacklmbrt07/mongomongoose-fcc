48d7f5faa48a8cf448281cc4be1acd34f00d9f0b
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-04 11:33:25 -0800

    completed challenge 9/12

--- myApp.js
+++ myApp.js
@@ -95,7 +95,18 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    (err, data) => {
+      if (err) {
+        return console.log(err);
+      } else {
+        done(null, data);
+      }
+    }
+  );
 };
 
 const removeById = (personId, done) => {
