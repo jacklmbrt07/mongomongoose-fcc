0c6fc90510122e76d69d7d1a86b007ae880f593e
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-04 11:40:47 -0800

    completed challenge 11/12

--- myApp.js
+++ myApp.js
@@ -118,8 +118,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, data) => {
+    if (err) return console.log;
+    done(null, data);
+  });
 };
 
 const queryChain = (done) => {
